d975a4d6b1b435dd174a08bb7259e91e3ef8161a
Marc G <marc@okidoo.ca>
2021-10-07 15:55:19 +0000

    deployment

--- client/docker/production/scripts/server.js
+++ client/docker/production/scripts/server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("/home/node/app/dist/client"));
+app.use(express.static("/home/node/app/dist/veterans"));
 
 app.get("/*", function (_, res) {
-  res.sendFile(path.join("/home/node/app/dist/client/index.html"));
+  res.sendFile(path.join("/home/node/app/dist/veterans/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
